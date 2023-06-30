2a6b361cba5d898bac746b0c3c6f5b26de81bb2e
nitramic <martin.cima@gmail.com>
2023-06-30 11:58:58 +0200

    config tomcat

---
+++ zi41v04/tm/setenv.sh
@@ -0,0 +1,13 @@
+export JAVA_HOME="/opt/jdk" 
+export CATALINA_HOME="/opt/tomcat"
+export LANG=it_IT@euro
+
+JAVA_OPTS="-Djava.awt.headless=true \
+-Dfile.encoding=UTF-8 \
+-Xms1028m \
+-Xmx4096m \
+-XX:MetaspaceSize=1024m \
+-XX:MaxMetaspaceSize=1024m \
+-XX:+HeapDumpOnOutOfMemoryError \
+-XX:HeapDumpPath=/opt/tomcat/logs \
+-XX:ErrorFile=/opt/tomcat/logs/java_error%p.log" 
