ff938b8eb9c62df56ecda4c69b2771b95fc47093
nitramic <martin.cima@gmail.com>
2023-11-22 16:33:37 +0100

    watchtower initial config

--- zi41v06/tm/DBConfig.sh
+++ zi41v06/tm/DBConfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cat <<EOF >/usr/local/tomcat/webapps/ahrw41_demo/WEB-INF/classes/DBConfig
 ### Auto generated file ###
-JDBCConnectionURL=jdbc:postgresql://${POSTGRES_IP}:5432/${POSTGRES_DB}
-JDBCDriver=org.postgresql.Driver
-UserName=${POSTGRES_USER}
-Password=${POSTGRES_PASSWORD}
+JDBCConnectionURL.1=jdbc:postgresql://${POSTGRES_IP}:5432/${POSTGRES_DB}
+JDBCDriver.1=org.postgresql.Driver
+UserName.1=${POSTGRES_USER}
+Password.1=${POSTGRES_PASSWORD}
 DefaultUserTimeout=3600
 EOF
